205a52b100333fe711ea251fe2daf05b93f8fa96
addityaasharma <adityasharma96655@gmail.com>
2025-05-06 16:20:04 +0530

    changes done

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="lg:max-w-[80vw] mx-auto w-[90vw]">
+  <div class="lg:max-w-[80vw] lg:mx-auto">
   <RouterView />
 
   </div>
